4d24b5fa5634e4311fde11148e39a12084f5f0a6
MykolaRyb97 <koljarubak1997@gmail.com>
2025-11-23 22:03:41 +0200

    оновлений код, який запрацював

--- services/backend/src/settings/config.py
+++ services/backend/src/settings/config.py
@@ -1,17 +1,17 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parents[4]
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 
 class AppConfig(BaseSettings):
     username: str | None = None
     password: str | None = None
 
-    IMAGE_DIR: Path
-    LOG_DIR: Path
-    WEB_SERVER_WORKERS: int
-    WEB_SERVER_START_PORT: int
+    IMAGE_DIR: Path = Path("/usr/src/images")
+    LOG_DIR: Path = Path("/usr/src/logs")
+    WEB_SERVER_WORKERS: int = 10
+    WEB_SERVER_START_PORT: int = 8000
 
     MAX_FILE_SIZE: int = 5 * 1024 * 1024
     SUPPORTED_FORMATS: set[str] = {'.jpg', '.png', '.gif'}
